b882fe12ef5a32b13942f98ae8c52bcd69f99f24
Brad Wise <>
2021-04-02 16:41:13 -0500

    Add env var check for aws execution type

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 
+import os
 from flask import Flask
 
 app = Flask(__name__)
 @app.route("/")
 def main():
-    return "Hello World ... welcome to Flask!"
+    if 'AWS_EXECUTION_ENV' in os.environ:
+        exeenv = os.getenv('AWS_EXECUTION_ENV')
+    else:
+        exeenv = "NOT FOUND"
+    return "Hello World ... welcome to Flask!  AWS_EXECUTION_ENV=%s" % exeenv
 
 if __name__ == "__main__":
     app.run()
