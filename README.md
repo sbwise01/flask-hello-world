11e3e50012c91f16a80c2f44fee908b9d42031c9
Brad Wise <>
2021-04-10 18:25:01 -0500

    Add path wildcarding and app name

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Simple flask app in a docker container.
 1. `wget https://github.com/gliderlabs/docker-alpine/raw/2bfe6510ee31d86cfeb2f37587f4cf866f28ffbc/versions/library-3.8/x86_64/rootfs.tar.xz`
 1. `docker build -t registry.infra.fogops.io/<your user/repo here>/flaskhelloworld .`
 1. `docker push registry.infra.fogops.io/<your user/repo here>/flaskhelloworld`
+
+## Run instructions
+`docker run -d --rm -e  APP_NAME=blue -p 5000:5000 sbwise/flaskhelloworld:0.1.5`
--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python
 
 import os
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
-@app.route("/")
-def main():
+@app.route('/', defaults={'u_path': ''})
+@app.route('/<path:u_path>')
+def main(u_path):
     if 'AWS_EXECUTION_ENV' in os.environ:
         exeenv = os.getenv('AWS_EXECUTION_ENV')
     else:
         exeenv = "NOT FOUND"
-    return "Hello World ... welcome to Flask!  AWS_EXECUTION_ENV=%s" % exeenv
+    if 'APP_NAME' in os.environ:
+        appName = os.getenv('APP_NAME')
+    else:
+        appName = "N/A"
+
+    return "Hello World ... welcome to Flask!  AWS_EXECUTION_ENV=%s, APP_NAME=%s, Path=%s" % (exeenv,appName,u_path)
 
 if __name__ == "__main__":
     app.run()
