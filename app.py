365b27330991051720697b3c2868645d7895b0df
Mark Mastoras <mark.mastoras@foghornconsulting.com>
2023-03-10 13:35:36 -0700

    add API_KEY environment lookup

--- app.py
+++ app.py
@@ -15,8 +15,13 @@ def main(u_path):
         appName = os.getenv('APP_NAME')
     else:
         appName = "N/A"
+        
+    if 'API_KEY' in os.environ:
+        apiKey = os.getenv('API_KEY')
+    else:
+        apiKey = "NOT FOUND"
 
-    return "Hello World ... welcome to Flask!  AWS_EXECUTION_ENV=%s, APP_NAME=%s, Path=%s" % (exeenv,appName,u_path)
+    return "Hello World ... welcome to Flask!  AWS_EXECUTION_ENV=%s, APP_NAME=%s, Path=%s API_KEY=%s" % (exeenv,appName,u_path,apiKey)
 
 if __name__ == "__main__":
     app.run()
